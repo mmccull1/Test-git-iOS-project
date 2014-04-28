096cf74f5b14a44bc70ed442fed882bffc65b13a
Mitchell McCulloch <Macaroon@Macaroons-Mac-mini.local>
2014-04-28 14:02:12 -0400

    Added a test class

---
+++ GitTest/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  GitTest
+//
+//  Created by Macaroon on 2014-04-28.
+//  Copyright (c) 2014 Planar Entertainment. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ GitTest/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  GitTest
+//
+//  Created by Macaroon on 2014-04-28.
+//  Copyright (c) 2014 Planar Entertainment. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
